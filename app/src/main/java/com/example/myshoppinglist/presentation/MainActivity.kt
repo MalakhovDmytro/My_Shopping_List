c28fe24b7eff24f9848a59cf8d8e1ad38b2d1f32
Dima Malakhov <malakhov.dmytro.s@gmail.com>
2023-05-24 21:28:35 +0200

    Added repository

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.myshoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myshoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  viewModel: MainViewModel
+    private lateinit var adapter: ShopListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivity", it.toString())
+            adapter.shopList = it
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shopList)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
 }
