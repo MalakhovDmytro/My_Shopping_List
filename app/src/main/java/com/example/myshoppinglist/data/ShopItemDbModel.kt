b7bd6fb17ecbdeb781e7bda9ece6cf3667b76ba1
malak <malakhov.dmytro.s@gmail.com>
2023-07-17 20:47:14 +0200

    Added fragment

---
+++ app/src/main/java/com/example/myshoppinglist/data/ShopItemDbModel.kt
@@ -0,0 +1,15 @@
+package com.example.myshoppinglist.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.myshoppinglist.domain.ShopItem
+@Entity(tableName = "shop_items")
+data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val enabled: Boolean
+) {
+
+}
